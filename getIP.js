5805fdf3abbd76b9b612e35cb0940a0187b90f4e
tuliopss <103906403+tuliopss@users.noreply.github.com>
2022-11-05 14:56:21 -0300

    Add files via upload

---
+++ getIP.js
@@ -0,0 +1,16 @@
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null);
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        const familyV4Value = typeof net.family === 'string' ? 'IPV4' : 4
+        if (net.family === familyV4Value && net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
