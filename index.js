eecbadf6dc9577216ca4663e732ad2297e34c3ee
Túlio <tuliovictorp@hotmail.com>
2022-11-06 14:35:14 -0300

    restaurante c node. mudanca de ideia

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const homehtml = fs.readFileSync('telaInicial.html');
+const cardapio = fs.readFileSync('cafemenu.html');
+const csscardapio = fs.readFileSync('style.css');
+
+//const getip = require('./getIP');
+
 
 app.listen(8888, function(erro) {
     if (erro) {
@@ -15,4 +20,8 @@ app.get("/", function(request, response) {
     response.end(homehtml);
 })
 
-//teste
+app.get("/cardapio", function(request, response){
+    response.end('/CafeMenu');
+    
+})
+
