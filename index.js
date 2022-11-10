036ada4c250763e5615438ad15310a3008c6aaef
Túlio <tuliovictorp@hotmail.com>
2022-11-10 15:55:34 -0300

    telas de rotas e .render

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const { text } = require('express');
 const express = require('express');
-const path = require('path');
-const app = express();
+var path = require('path');
+//const session = require('express-session')
 const fs = require('fs');
-const login = fs.readFileSync('login.html');
+
+const app = express();
+
+//const login = fs.readFileSync('login.html');
 const cardapio = fs.readFileSync('cafemenu.html');
 const home = fs.readFileSync('index.html');
-app.use(express.static(__dirname + '/public')) //upar css e js
 
-//app.use(express.static('css'));
-//app.static('/css', '/Nuvem/css/styles.css');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'))
+
+//app.use(session({secret: 'aspodjkaopijfde'}));
+app.use(express.static(__dirname + '/public')); //upar css e js
+app.use(express.static(__dirname + '/CafeMenu')); //upar css e js
+
+
+
 
 
 
@@ -22,27 +32,33 @@ app.listen(8888, function(erro) {
 })
 
 app.get("/login", function(request, response) {
-     response.end(login);
+    response.render('login');
 
     let email = request.query.email;
     let senha = request.query.senha;
 
     if (email == undefined && senha == undefined) {
+        console.log('email, senha');
         return response.send('Insira seu email e senha');
+
     } else if (email == undefined && senha != undefined) {
+        console.log(senha)
         return response.send('Informe seu email');
+
     } else if (email != undefined && senha == undefined) {
+        console.log(email)
         return response.send('Insira sua senha');
+
     }  else if (email != undefined && senha != undefined) {
+        console.log(email, senha)
         return response.send('Seja bem vindo!');
-        //redireciona para o /home
+        
     }
 })
 
 
 app.get("/", function(request, response){
     response.sendFile(path.join(__dirname + '/index.html'))
-    //Imagem simbólica do restaurante. Não sai junto do arquivo html
     
 })
 
@@ -50,8 +66,7 @@ app.get("/", function(request, response){
 app.get("/cardapio", function(request, response){
     const itens = ["French Vanilla", "Caramel Macchiato", "Pumpkin Spice ", "Hazelnut", "Mocha"];
     //console.log(itens);
-    response.end(cardapio);
-
+    response.render('cafemenu');
 
 })
 
@@ -79,7 +94,7 @@ app.get("/cardapio/pedido/:index", function(request, response){
 
     const {index} = request.params;
 
-
+    
     response.send("Escolha o seu café:" + itens)    
     console.log('Você escolheu o café: ' + itens[index])                                                                            //" <br><input type='text' id='pedido'> " + " <br><button onclick='pedido()'>Escolher</button>") //concatenar. nao envia 2 sends
  
