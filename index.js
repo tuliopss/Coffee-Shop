e6fe9a725a5886e4bcf19b088e294c1b04234289
Túlio <tuliovictorp@hotmail.com>
2022-11-11 15:07:35 -0300

    criacao do back da rota de login

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { text } = require('express');
+const { text, response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 var path = require('path');
-//const session = require('express-session')
+const session = require('express-session')
 const fs = require('fs');
 
 const app = express();
@@ -14,7 +15,8 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '/views'))
 
-//app.use(session({secret: 'aspodjkaopijfde'}));
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public')); //upar css e js
 app.use(express.static(__dirname + '/CafeMenu')); //upar css e js
 
@@ -31,33 +33,37 @@ app.listen(8888, function(erro) {
     }
 })
 
-app.get("/login", function(request, response) {
+app.get('/cadastro', function(request, response){
+    response.render('cadastro.html')
+})
+
+
+
+  app.post('/login', function(request, response) {
+var login = 'admin';
+var password = 123;
+    if (request.body.login == login && request.body.password == password) {
+        //Logado c sucesso
+        //request.session.login = login;
+        console.log('logado')
+        response.redirect('/home')
+    } else {
+    response.render('login')
+    }
+
+})
+
+app.get('/login', function (request, response) {
     response.render('login');
 
-    let email = request.query.email;
-    let senha = request.query.senha;
+    if (request.body.session) {
+        response.sendFile(path.join(__dirname + '/index.html'))
 
-    if (email == undefined && senha == undefined) {
-        console.log('email, senha');
-        return response.send('Insira seu email e senha');
-
-    } else if (email == undefined && senha != undefined) {
-        console.log(senha)
-        return response.send('Informe seu email');
-
-    } else if (email != undefined && senha == undefined) {
-        console.log(email)
-        return response.send('Insira sua senha');
-
-    }  else if (email != undefined && senha != undefined) {
-        console.log(email, senha)
-        return response.send('Seja bem vindo!');
-        
     }
 })
 
 
-app.get("/", function(request, response){
+app.get("/home", function(request, response){
     response.sendFile(path.join(__dirname + '/index.html'))
     
 })
@@ -77,7 +83,7 @@ app.get("/cardapio/pedido", function(request, response){
      " 3 -Hazelnut", 
      " 4 - Mocha"];
 
-    
+    //response.render('cafemenu');
     response.send("Escolha o seu café:" + itens)    
     console.log(itens)                                                                            //" <br><input type='text' id='pedido'> " + " <br><button onclick='pedido()'>Escolher</button>") //concatenar. nao envia 2 sends
  
@@ -94,6 +100,7 @@ app.get("/cardapio/pedido/:index", function(request, response){
 
     const {index} = request.params;
 
+    //response.render('cafemenu')
     
     response.send("Escolha o seu café:" + itens)    
     console.log('Você escolheu o café: ' + itens[index])                                                                            //" <br><input type='text' id='pedido'> " + " <br><button onclick='pedido()'>Escolher</button>") //concatenar. nao envia 2 sends
